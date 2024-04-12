97694f282f00a11767d02b2251a416d2056d1b99
Fredrik Sundberg <ted.sundberg@gmail.com>
2024-04-12 20:59:17 +0200

    feat: Better optimization - use factory pattern to avoid recreating array on each function call

--- optimize.js
+++ optimize.js
@@ -3,7 +3,7 @@ const ARR_LENGTH = 1000000;
 // original function
 export function find1(index) {
   let a = [];
-  for (let i = 0; i < ARR_LENGTH; i++) {
+  for (let i = 0; i < 1000000; i++) {
     a[i] = i * i;
   }
   return a[index];
@@ -18,18 +18,14 @@ export function find2(index) {
   return a[index];
 }
 
-// alternatively, if we can assume the length of the array will not be larger than the index, we can avoid pupulating the array for greter values than index
-export function find3(index) {
-  const arrayLength = index + 1;
-
-  let a = new Array(arrayLength);
-  for (let i = 0; i < arrayLength; i++) {
+// optimization 2: use a factory function to avoid recreating the array every time the function is called
+export function createFind() {
+  let a = [];
+  for (let i = 0; i < ARR_LENGTH; i++) {
     a[i] = i * i;
   }
-  return a;
-}
 
-// if we can assume that this function is a simple calculation, we do not even need to allocate the array
-export function find4(index) {
-  return index * index;
+  return function find(index) {
+    return a[index];
+  };
 }
--- optimize.test.js
+++ optimize.test.js
@@ -1,19 +1,32 @@
-import { find1, find2, find3, find4 } from "./optimize";
+import { find1, find2, createFind } from "./optimize";
 import { expect, test } from "vitest";
 
 const timeExecution = (fn, args) => {
   const start = Date.now();
   fn(args);
   const end = Date.now();
-  console.log(`${fn.name} took ${end - start}ms`);
   return end - start;
 };
 
 test("execution time is lower for the optimized functions", () => {
   const args = 100;
-  const functions = [find1, find2, find3, find4];
+  const functions = [find1, find2];
   const times = functions.map((fn) => timeExecution(fn, args));
 
-  console.log(times[1], "vs", times[0], "ms");
   expect(times[1]).toBeLessThan(times[0]);
+});
+
+test("execution time is lower for the optimized functions", () => {
+  const time1 = timeExecution(() => {
+    find1(6);
+    find1(12);
+  });
+
+  const time2 = timeExecution(() => {
+    const optimized = createFind();
+    optimized(6);
+    optimized(12);
+  });
+
+  expect(time2).toBeLessThan(time1);
 });
